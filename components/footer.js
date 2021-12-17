641218475c6af4058ff4e6bcaaf946fa930bf64e
Nawrasse Dahman <nawrassehimself@pm.me>
2021-12-17 22:11:17 +0100

    Update footer.js

--- components/footer.js
+++ components/footer.js
@@ -1,10 +1,14 @@
-import { Box } from '@chakra-ui/react'
+import { Box, Divider } from '@chakra-ui/react'
+import Section from '../components/section'
 
 const Footer = () => {
   return (
     <Box align="center" opacity={0.4} fontSize="sm">
+    <Section delay={0.4}> 
+    <Divider my={6} />
       &copy; {new Date().getFullYear()} Nawrasse Dahman. All Rights Reserved.
     </Box>
+    </section>
   )
 }
 
