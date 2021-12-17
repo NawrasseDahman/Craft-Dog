ef77a9197b435fd3650d627ac2b15ff9de1c41d1
Nawrasse Dahman <nawrassehimself@pm.me>
2021-12-18 00:04:10 +0100

    Update business.js

--- pages/business.js
+++ pages/business.js
@@ -36,29 +36,7 @@ const Business = () => (
           </Heading>
           <p>Digital Craftsman ( Developer / Designer )</p>
         </Box>
-        <Box
-          flexShrink={0}
-          mt={{ base: 4, md: 0 }}
-          ml={{ md: 6 }}
-          textAlign="center"
-        ></Box>
-      </Box>        
-        <Box
-          flexShrink={0}
-          mt={{ base: 4, md: 0 }}
-          ml={{ md: 6 }}
-          textAlign="center"
-        ></Box>
-         <Image
-            borderColor="whiteAlpha.800"
-            borderWidth={2}
-            borderStyle="solid"
-            maxWidth="100px"
-            display="inline-block"
-            borderRadius="full"
-            src="/nawrasse.png"
-            alt="Profile image"
-          />
+      </Box>
       <Section delay={0.1}>
         <Heading as="h3" variant="section-title">
           Business Offers
