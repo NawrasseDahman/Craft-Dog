53609216ebe1d2f3575b60d0efdd508111787c8e
Nawrasse Dahman <nawrassehimself@pm.me>
2021-12-18 18:01:41 +0100

    Update work.js

--- components/work.js
+++ components/work.js
@@ -1,6 +1,6 @@
 import NextLink from 'next/link'
 import { Heading, Box, Image, Link, Badge } from '@chakra-ui/react'
-import { ChevronRightIcon } from '@chakra-ui/icons'
+import { FaRegCaretSquareRight } from "react-icons/fa";
 
 export const Title = ({ children }) => (
   <Box>
@@ -9,7 +9,7 @@ export const Title = ({ children }) => (
     </NextLink>
     <span>
       {' '}
-      <ChevronRightIcon />{' '}
+      <FaRegCaretSquareRight />{' '}
     </span>
     <Heading display="inline-block" as="h3" fontSize={20} mb={4}>
       {children}
