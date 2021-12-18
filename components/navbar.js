c881c7b09814aee812a073e5c75476f40540bfa3
Nawrasse Dahman <nawrassehimself@pm.me>
2021-12-18 17:06:58 +0100

    Update navbar.js

--- components/navbar.js
+++ components/navbar.js
@@ -16,7 +16,7 @@ import {
 } from '@chakra-ui/react'
 import { HamburgerIcon } from '@chakra-ui/icons'
 import ThemeToggleButton from './theme-toggle-button'
-import { FaCodepen, FaSpotify, FaEnvelopeOpenText, FaBuffer } from 'react-icons/fa';
+import { FaCodepen, FaSpotify, FaUserAstronaut, FaBuffer } from 'react-icons/fa';
 
 const LinkItem = ({ href, path, _target, children, ...props }) => {
   const active = path === href
@@ -80,7 +80,7 @@ const Navbar = props => {
             style={{ gap: 4 }}
             pl={2}
           >
-            <FaEnvelopeOpenText />
+            <FaUserAstronaut />
             Business
           </LinkItem>
           <LinkItem 
