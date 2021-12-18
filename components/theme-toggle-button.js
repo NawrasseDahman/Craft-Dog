1736f9dac4613d4bb3f7de4e017f8c4e68641843
Nawrasse Dahman <nawrassehimself@pm.me>
2021-12-18 20:25:04 +0100

    Update theme-toggle-button.js

--- components/theme-toggle-button.js
+++ components/theme-toggle-button.js
@@ -17,7 +17,7 @@ const ThemeToggleButton = () => {
       >
         <IconButton
           aria-label="Toggle theme"
-          colorScheme={useColorModeValue('purple', 'orange')}
+          colorScheme={useColorModeValue('gray', 'orange')}
           icon={useColorModeValue(<FaRegMoon />, <FaRegSun />)}
           onClick={toggleColorMode}
         ></IconButton>
