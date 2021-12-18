e78c4d8b469a572032affc2d5fa74451ba4248ac
Nawrasse Dahman <nawrassehimself@pm.me>
2021-12-18 21:25:30 +0100

    Update navbar.js

--- components/navbar.js
+++ components/navbar.js
@@ -26,7 +26,7 @@ const LinkItem = ({ href, path, _target, children, ...props }) => {
       <Link
         p={2}
         bg={active ? 'grassTeal' : undefined}
-        color={active ? '#086d78' : inactiveColor}
+        color={active ? '#202023' : inactiveColor}
         _target={_target}
         {...props}
       >
