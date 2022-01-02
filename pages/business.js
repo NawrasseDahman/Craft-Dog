d431a3a1e458a2feacc8dd2d96fa4962ca911aed
Nawrasse Dahman <nawrassehimself@pm.me>
2022-01-02 08:34:20 +0100

    Create business.js

---
+++ pages/business.js
@@ -0,0 +1,165 @@
+import NextLink from "next/link";
+import {
+  Link,
+  Container,
+  Heading,
+  Box,
+  Button,
+  List,
+  ListItem,
+  Icon,
+  useColorModeValue
+} from "@chakra-ui/react";
+import {
+  SiAmazonaws,
+  SiBabel,
+  SiDocker,
+  SiGit,
+  SiGo,
+  SiJava,
+  SiNextdotjs,
+  SiNodedotjs,
+  SiPostgresql,
+  SiReact,
+  SiRedis,
+  SiStyledcomponents,
+  SiTailwindcss,
+  SiTypescript,
+  SiWebpack,
+  SiWebstorm,
+  SiMongodb,
+  SiYarn
+} from "react-icons/si";
+import { FaRegCaretSquareRight } from "react-icons/fa";
+import Paragraph from "../components/paragraph";
+import Layout from "../components/layouts/article";
+import Section from "../components/section";
+import { FaTwitter, FaGithub, FaDiscord, FaMastodon } from "react-icons/fa";
+
+const Business = () => (
+  <Layout>
+    <Container>
+      <Box
+        borderRadius="lg"
+        mb={6}
+        p={3}
+        textAlign="center"
+        bg={useColorModeValue("whiteAlpha.500", "whiteAlpha.200")}
+      >
+        Hello, I&apos;m A Full-Stack Developer Who Is A Movie Junkie!
+      </Box>
+
+      <Box display={{ md: "flex" }}>
+        <Box flexGrow={1}>
+          <Heading as="h2" variant="page-title">
+            If You Work, It Will Work.
+          </Heading>
+          <p>Digital Craftsman ( Developer / Designer )</p>
+        </Box>
+      </Box>
+      <Section delay={0.1}>
+        <Heading as="h3" variant="section-title">
+          Business Offers
+        </Heading>
+        <Paragraph>
+          I&apos;m Currently Open To Freelance And Actively Looking For A
+          Full-Time Job Opportunties, So If You Like What You&apos;ve Seen
+          Please Don&apos;t Hesitate To Get In Touch! If You Have Any Questions
+          Or Would Just Like To Say Hi, You Can Dm Me On Any Of My Socials Below
+          Or Send Me A Friend Request In Discord, I&apos;ll Delighted To Get
+          Back To You As Soon As Possible.
+        </Paragraph>
+        <Box align="center" my={4}>
+          <NextLink href="/images/cv.pdf">
+            <Button rightIcon={<FaRegCaretSquareRight />} colorScheme="teal">
+              Download My Resume
+            </Button>
+          </NextLink>
+        </Box>
+      </Section>
+
+      <Section delay={0.3}>
+        <Heading as="h3" variant="section-title">
+          Technologies
+        </Heading>
+        <ul>
+          <ListItem icon={SiDocker} text="Docker" />
+          <ListItem icon={SiRedis} text="Redis" />
+          <ListItem icon={SiPostgresql} text="Postgres" />
+          <ListItem icon={SiReact} text="React.js" />
+          <ListItem icon={SiNodedotjs} text="Node.js" />
+          <ListItem icon={SiTypescript} text="TypeScript" />
+          <ListItem icon={SiGo} text="Golang" />
+          <ListItem icon={SiJava} text="Java" />
+          <ListItem icon={SiAmazonaws} text="AWS" />
+          <ListItem icon={SiWebstorm} text="WebStorm" />
+          <ListItem icon={SiNextdotjs} text="Next.js" />
+          <ListItem icon={SiWebpack} text="Webpack" />
+          <ListItem icon={SiBabel} text="Babel" />
+          <ListItem icon={SiYarn} text="Yarn" />
+          <ListItem icon={SiTailwindcss} text="TailwindCSS" />
+          <ListItem icon={SiGit} text="Git" />
+          <ListItem icon={SiStyledcomponents} text="styled-components" />
+          <ListItem icon={SiMongodb} text="Mongo" />
+        </ul>
+      </Section>
+
+      <Section delay={0.5}>
+        <Heading as="h3" variant="section-title">
+          Social Media
+        </Heading>
+        <List>
+          <ListItem>
+            <Link href="https://github.com/NawrasseDahman" target="_blank">
+              <Button
+                variant="ghost"
+                colorScheme="teal"
+                leftIcon={<Icon as={FaGithub} />}
+              >
+                @NawrasseDahman
+              </Button>
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link href="https://twitter.com/nawrassedahman" target="_blank">
+              <Button
+                variant="ghost"
+                colorScheme="teal"
+                leftIcon={<Icon as={FaTwitter} />}
+              >
+                @NawrasseDahman
+              </Button>
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link href="mailto:nawrassehimself@gmail.com" target="_blank">
+              <Button
+                variant="ghost"
+                colorScheme="teal"
+                leftIcon={<Icon as={FaMastodon} />}
+              >
+                @NawrasseDahman
+              </Button>
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link
+              href="https://discord.com/users/878309088126197860"
+              target="_blank"
+            >
+              <Button
+                variant="ghost"
+                colorScheme="teal"
+                leftIcon={<Icon as={FaDiscord} />}
+              >
+                @NawrasseDahman
+              </Button>
+            </Link>
+          </ListItem>
+        </List>
+      </Section>
+    </Container>
+  </Layout>
+);
+
+export default Business;
