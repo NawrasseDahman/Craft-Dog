5c07aaff0563d2b6782fedd55e318bbd83a95775
Nawrasse Dahman <nawrassehimself@pm.me>
2022-01-09 12:36:48 +0100

    Create index.js

---
+++ components/index.js
@@ -0,0 +1,42 @@
+const techStacks = [
+    {
+        name: "Android",
+        icon: <FaAndroid fontSize="20px" />,
+        url: "https://www.android.com/",
+    },
+    {
+        name: "React Native",
+        icon: <FaReact fontSize="20px" />,
+        url: "https://reactnative.dev/",
+    },
+    {
+        name: "Kotlin",
+        icon: <SiKotlin fontSize="20px" />,
+        url: "https://kotlinlang.org/",
+    },
+    {
+        name: "Java",
+        icon: <FaJava fontSize="20px" />,
+        url: "https://www.java.com/en/",
+    },
+    {
+        name: "Javascript",
+        icon: <FaJs fontSize="20px" />,
+        url: "https://www.javascript.com/",
+    },
+    {
+        name: "Firebase",
+        icon: <SiFirebase fontSize="20px" />,
+        url: "https://firebase.google.com/",
+    },
+    {
+        name: "Github & GitLab",
+        icon: <FaGitAlt fontSize="20px" />,
+        url: "https://www.gitlab.com/",
+    },
+    {
+        name: "CI/CD - Fastlane",
+        icon: <SiFastlane fontSize="20px" />,
+        url: "https://fastlane.tools/",
+    },
+]
