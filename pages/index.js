ad06f7075b97e26e50267a4aa9a839ccc25cb2bc
Nawrasse Dahman <nawrassehimself@gmail.com>
2022-02-04 13:34:30 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -18,6 +18,47 @@ import Section from '../components/section'
 import { SiCodepen, SiInstagram, SiGithub, SiSpotify } from 'react-icons/si';
 import { FaRegCaretSquareRight } from "react-icons/fa";
 import { age } from '../components/time';
+import React, { useEffect, useState } from "react";
+import ReactLoading from "react-loading";
+
+function Loader() {
+  const [data, setData] = useState([]);
+  const [done, setDone] = useState(undefined);
+
+  useEffect(() => {
+    setTimeout(() => {
+      fetch("https://nawrasse.netlify.app/")
+        .then((response) => response.json())
+        .then((json) => {
+          console.log(json);
+          setData(json);
+          setDone(true);
+        });
+    }, 2000);
+  }, []);
+
+  return (
+    <>
+      {!done ? (
+        <ReactLoading
+          type={"bars"}
+          color={"#ffda96"}
+          height={100}
+          width={100}
+        />
+      ) : (
+        <ul>
+          {data.map((post) => (
+            <li key={post.id}>{post.title}</li>
+          ))}
+        </ul>
+      )}
+    </>
+  );
+}
+
+export default Loader;
+
 
 const Home = () => (
   <Layout>
