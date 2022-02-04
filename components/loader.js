bc61d284a9689a130e0da0897b4c1a8e61435d94
Nawrasse Dahman <nawrassehimself@gmail.com>
2022-02-04 12:42:23 +0100

    Create loader.js

---
+++ components/loader.js
@@ -0,0 +1,15 @@
+import { XlviLoader } from "react-awesome-loaders";
+
+export const XlviLoaderComponent = () => {
+  return (
+    <>
+      <XlviLoader
+        boxColors={["#EF4444", "#F59E0B", "#6366F1"]}
+        desktopSize={"128px"}
+        mobileSize={"100px"}
+      />
+    </>
+  );
+};
+
+export default XlviLoaderComponent
