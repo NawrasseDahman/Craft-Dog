a712aeb7551a6098c45c38ce1f3e92370066104a
Nawrasse Dahman <nawrassehimself@gmail.com>
2022-02-04 20:02:28 +0100

    Update footer.js

--- components/footer.js
+++ components/footer.js
@@ -2,6 +2,32 @@ import { Box, Divider } from '@chakra-ui/react'
 import Section from '../components/section'
 
 const Footer = () => {
+  
+  <>
+  <p />
+ <p />
+  <table width="100%" height="100%">
+    <tbody>
+      <tr>
+        <td width="100%" height="100%">
+          <p />
+          <p>
+            <br />{" "}
+            <a href="https://github.com/kittinan/spotify-github-profile">
+              <img
+                src="https://spotify-github-profile.vercel.app/api/view?uid=4pevqlgntbtzf7quj0q307245&cover_image=true&theme=novatorem&bar_color=53b14f&bar_color_cover=true"
+                alt="spotify-github-profile"
+              />
+           </a>
+          </p>
+          <p></p>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+  <table></table>
+</>
+  
   return (
     <Box align="center" opacity={0.4} fontSize="sm">
     <Section delay={0.4} />
