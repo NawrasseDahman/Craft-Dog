f4521907e496483034b509365abdd77c4687b35f
Nawrasse Dahman <nawrassehimself@gmail.com>
2022-02-04 19:28:41 +0100

    Update footer.js

--- components/footer.js
+++ components/footer.js
@@ -1,8 +1,18 @@
 import { Box, Divider } from '@chakra-ui/react'
 import Section from '../components/section'
+import Layout from '../components/Layout'
 
 const Footer = () => {
   return (
+    
+
+export const meta = {
+  title: 'Markdown/MDX with Next.js',
+  author: 'Lee Robinson'
+}
+
+hi
+
     <Box align="center" opacity={0.4} fontSize="sm">
     <Section delay={0.4} />
     <Divider my={6} />
@@ -11,4 +21,4 @@ const Footer = () => {
   )
 }
 
-export default Footer
+export default Footer ({ children }) => <Layout meta={meta}>{children}</Layout>
