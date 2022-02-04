f7b4d973f584d93dc199f0a0e9f0b2918c3eafc3
Nawrasse Dahman <nawrassehimself@gmail.com>
2022-02-04 14:35:45 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -21,14 +21,13 @@ import { age } from '../components/time';
 import React from 'react';
 import { BarWave } from "react-cssfx-loading";
 
-
-const Home = () => (
-   
-  <>
+<>
   <BarWave />
   <BarWave color="#ffda96" width="100px" height="100px" duration="5s" />
   <BarWave onClick={() => alert("Clicked")} key="key" />
-  </>
+</>
+
+const Home = () => (
 
   <Layout>
     <Container>
