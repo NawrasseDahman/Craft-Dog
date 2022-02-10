2c7140e270ff17111d6eeeb049ab112d4b3af404
Nawrasse Dahman <nawrassehimself@gmail.com>
2022-02-10 16:29:42 +0100

    Update navbar.js

--- components/navbar.js
+++ components/navbar.js
@@ -119,7 +119,7 @@ const Navbar = (props) => {
             Business
           </LinkItem>
         </Stack>
-        <Box flex={1} align="left">
+        <Box flex={1} align="right">
           <ThemeToggleButton />
 
           <Box ml={2} display={{ base: "inline-block", md: "none" }}>
