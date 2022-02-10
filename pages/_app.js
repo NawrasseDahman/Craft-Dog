bbe0fcb6444c62859f451317caae485f996618af
Nawrasse Dahman <nawrassehimself@gmail.com>
2022-02-10 20:16:54 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,17 +3,18 @@ import Layout from '../components/layouts/main'
 import Fonts from '../components/fonts'
 import theme from '../lib/theme'
 import { AnimatePresence } from 'framer-motion'
+import Chakra from '../components/chakra'
 
 function Website({ Component, pageProps, router }) {
   return (
-    <ChakraProvider theme={theme}>
+    <Chakra cookies={pageProps.cookies}>
       <Fonts />
       <Layout router={router}>
         <AnimatePresence exitBeforeEnter initial={true}>
           <Component {...pageProps} key={router.route} />
         </AnimatePresence>
       </Layout>
-    </ChakraProvider>
+    </Chakra>
   )
 }
 
